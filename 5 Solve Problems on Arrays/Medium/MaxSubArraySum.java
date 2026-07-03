32c292c076f40aab2049969ae681021e246aee31
harshparmar70 <parmarharsh328@gmail.com>
2026-07-03 19:37:08 +0530

    Array

--- 5 Solve Problems on Arrays/Medium/MaxSubArraySum.java
+++ 5 Solve Problems on Arrays/Medium/MaxSubArraySum.java
@@ -15,12 +15,36 @@ class MaxSubArraySum {
         return maxiSum;
     }
 
+    static int maxSubArrayKadanes(int arr[]) {
+        int maxi = Integer.MIN_VALUE;
+        int sum = 0;
+        int ansStr = -1, ansEnd = -1;
+        int start = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (sum == 0) {
+                start = i;
+            }
+            sum += arr[i];
+            if (sum > maxi) {
+                maxi = sum;
+                ansStr = start;
+                ansEnd = i;
+            }
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        for (int i = ansStr; i <= ansEnd; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("");
+        return maxi;
+    }
+
     public static void main(String[] args) {
         int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
         System.out.println(maxSubArray(arr));
+        System.out.println(maxSubArrayKadanes(arr));
 
     }
 }
-git add .
-git commit -m"Array"
-git push origin main
