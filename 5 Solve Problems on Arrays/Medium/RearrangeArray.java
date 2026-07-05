5bc6cd2c6a68df21d294cf4773d771562674eb97
harshparmar70 <parmarharsh328@gmail.com>
2026-07-05 16:59:30 +0530

    Array

--- 5 Solve Problems on Arrays/Medium/RearrangeArray.java
+++ 5 Solve Problems on Arrays/Medium/RearrangeArray.java
@@ -1,4 +1,6 @@
 
+import java.util.ArrayList;
+
 class RearrangeArray {
 
     static int[] rearrangeArray(int arr[]) {
@@ -18,11 +20,48 @@ class RearrangeArray {
         return ans;
     }
 
-    public static void main(String[] args) {
-        int[] A = {1, 2, -4, -5};
-        int result[] = rearrangeArray(A);
+    static void varieties2(int arr[]) {
+        int n = arr.length;
+        ArrayList<Integer> pos = new ArrayList<>();
+        ArrayList<Integer> neg = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > 0) {
+                pos.add(arr[i]);
+            } else {
+                neg.add(arr[i]);
+            }
+        }
 
-        for (int num : result) {
+        if (pos.size() > neg.size()) {
+            for (int i = 0; i < neg.size(); i++) {
+                arr[i * 2] = pos.get(i);
+                arr[i * 2 + 1] = neg.get(i);
+            }
+            int index = neg.size() * 2;
+            for (int i = neg.size(); i < pos.size(); i++) {
+                arr[index] = pos.get(i);
+                index++;
+            }
+        } else {
+            for (int i = 0; i < pos.size(); i++) {
+                arr[i * 2] = pos.get(i);
+                arr[i * 2 + 1] = neg.get(i);
+            }
+            int index = pos.size() * 2;
+            for (int i = pos.size(); i < neg.size(); i++) {
+                arr[index] = neg.get(i);
+                index++;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] A = {1, 2, -4, -5, 5, 9};
+        // int result[] = rearrangeArray(A);
+        varieties2(A);
+
+        for (int num : A) {
             System.out.print(num + " ");
         }
     }
