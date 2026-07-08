6a8038b670b4a797625b12279e0fc7f6a6be1e70
harshparmar70 <parmarharsh328@gmail.com>
2026-07-08 17:41:48 +0530

    Array

--- 5 Solve Problems on Arrays/Medium/RotateImage90degree.java
+++ 5 Solve Problems on Arrays/Medium/RotateImage90degree.java
@@ -15,6 +15,31 @@ public class RotateImage90degree {
 
     }
 
+    static void rotateClockwiseOpt(int arr[][]) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int tmp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = tmp;
+            }
+        }
+
+        //Reverse
+        for (int i = 0; i < n; i++) {
+            int left = 0;
+            int right = n - 1;
+            while (left < right) {
+                int tmp = arr[i][left];
+                arr[i][left] = arr[i][right];
+                arr[i][right] = tmp;
+                left++;
+                right--;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int[][] mat = {
             {1, 2, 3},
@@ -24,10 +49,12 @@ public class RotateImage90degree {
 
         int[][] rotated = rotateClockwise(mat);
         int n = rotated.length;
+
+        rotateClockwiseOpt(mat);
         // Print the rotated matrix
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.print(rotated[i][j] + " ");
+                System.out.print(mat[i][j] + " ");
             }
             System.out.println();
         }
