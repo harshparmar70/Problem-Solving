33e1a4207ad154772e69dcf2af070132095466e2
harshparmar70 <parmarharsh328@gmail.com>
2026-07-14 17:11:55 +0530

    BINARY SEARCH

--- 6 Binary Search/FirstandLastOccurrence.java
+++ 6 Binary Search/FirstandLastOccurrence.java
@@ -57,14 +57,63 @@ public class FirstandLastOccurrence {
         return ans;
     }
 
+    static int firstOccurrenceopt(int arr[], int n, int x) {
+        int low = 0, high = n - 1;
+        int first = n;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == x) {
+                first = mid;
+                high = mid - 1;
+            } else if (arr[mid] < n) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return first;
+
+    }
+
+    static int lastOccurrenceopt(int arr[], int n, int x) {
+
+        int low = 0, high = n - 1;
+        int last = n;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == x) {
+                last = mid;
+                low = mid + 1;
+            } else if (arr[mid] < n) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return last;
+    }
+
+    static int[] firstandLastOccurrenceopt2(int arr[], int n, int x) {
+
+        int first = firstOccurrenceopt(arr, n, x);
+        if (first == -1) {
+            return new int[]{-1, -1};
+        }
+        int last = lastOccurrenceopt(arr, n, x);
+        return new int[]{first, last};
+    }
+
     public static void main(String[] args) {
         int arr[] = {3, 4, 13, 13, 13, 20, 40};
         int x = 13;
         // int bf[] = firstandLastOccurrence(arr, x);
-        int bf[] = firstandLastOccurrenceopt(arr, arr.length, x);
+        int bf[] = firstandLastOccurrenceopt2(arr, arr.length, x);
         for (int elem : bf) {
             System.out.print(elem + " ");
         }
+        System.out.println("");
+        System.out.println(firstOccurrenceopt(arr, arr.length, x));
+        System.out.println(lastOccurrenceopt(arr, arr.length, x));
 
     }
 }
